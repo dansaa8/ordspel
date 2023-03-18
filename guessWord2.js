189cb0053697aad445834981f54c36b28ef56d79
Dan Saa <dansaa8@protonmail.com>
2023-03-18 15:12:43 +0100

    compress functionality, reduce code

--- guessWord2.js
+++ guessWord2.js
@@ -4,25 +4,32 @@ export default function guessWord2(target, guess) {
     guess = guess.toUpperCase().split('');
     target = target.toUpperCase().split('');
 
-    const charsInGuess = countLetters(guess);
-    const charsInTarget = countLetters(target);
+    const uniqueCharsInGuess = countLetters(guess);
+    const uniqueCharsInTarget = countLetters(target);
 
     const temp = [];
     for (let i = 0; i < guess.length; i++) {
-        console.log(charsInGuess);
         if (guess[i] === target[i]) {
             temp.push({ letter: guess[i], result: 'correct' });
-            charsInGuess.map(obj => { if(obj.letter === guess[i]) obj.count-- });
-            charsInTarget.map(obj => { if(obj.letter === guess[i]) obj.count-- });
+            decrementCharCount(guess[i]);
         }
         else if (!target.includes(guess[i])) {
             temp.push({ letter: guess[i], result: 'incorrect' });
         }
      else {
-           
+
     }
+
+    console.log('guess: ', uniqueCharsInGuess);
+    console.log('target: ', uniqueCharsInTarget);
     }
     return temp;
+
+    function decrementCharCount(letterToBeDecremented) {
+        uniqueCharsInGuess.map(obj => { if (obj.letter === letterToBeDecremented) obj.count-- });
+        uniqueCharsInTarget.map(obj => { if (obj.letter === letterToBeDecremented) obj.count-- });
+    }
 }
 
-console.log(guessWord2('Mars', 'Bars'));
+
+console.log(guessWord2('Marssss', 'Barssss'));
