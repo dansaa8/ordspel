f1cbaf64d03abd8f60068244678c2da9ac67bb43
Dan Saa <dansaa8@protonmail.com>
2023-04-17 21:06:58 +0200

    Create basic page-routing with ejs

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,19 +1,35 @@
 import express from 'express';
-// import siteNavigation from './routes/siteNavigation.js';
 import api from './routes/api.js';
 import highscores from './routes/highscores.js';
+import expressLayouts from 'express-ejs-layouts';
 
 
 const app = express();
+app.use(expressLayouts);
 app.use(express.json());
 app.set('view engine', 'ejs');
+// app.use(express.static('static'));
+
+app.get('/', (req, res) => {
+    res.render('', { title: 'Play Game' });
+})
+
+app.get('/highscore', async (req, res) => {
+    res.render('highscore', { title: 'Highscore' });
+})
+
+app.get('/about', async (req, res) => {
+    res.render('about', { title: 'About' });
+})
+
+
 
 // app.use('/', siteNavigation);
 app.use('/api/games', api);
 app.use('/api/highscores', highscores);
 
 //konfigurera express, så att den ska leta efter statiska i frontendmappens build mapp
-app.use(express.static('../frontend/build'));
+// app.use(express.static('../frontend/build'));
 
 
 
