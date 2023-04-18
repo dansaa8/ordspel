a29aeb2553724764abd3e614ae9bdab577c8a39d
Dan Saa <dansaa8@protonmail.com>
2023-04-18 12:11:00 +0200

    Add working route to get react-bundle from backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,26 +3,33 @@ import api from './routes/api.js';
 import highscores from './routes/highscores.js';
 import expressLayouts from 'express-ejs-layouts';
 
+import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(expressLayouts);
 app.use(express.json());
 app.set('view engine', 'ejs');
-// app.use(express.static('static'));
+// app.use(express.static('../../frontend/build/static'));
 
 app.get('/', (req, res) => {
-    res.render('', { title: 'Play Game' });
-})
+  res.render('', { title: 'Play Game' });
+});
 
 app.get('/highscore', async (req, res) => {
-    res.render('highscore', { title: 'Highscore' });
-})
+  res.render('highscore', { title: 'Highscore' });
+});
 
 app.get('/about', async (req, res) => {
-    res.render('about', { title: 'About' });
-})
+  res.render('about', { title: 'About' });
+});
 
-
+app.get('/bundle.js', async (req, res) => {
+  res.sendFile(path.join(__dirname, '../../frontend/build/static/js/main.2a60a447.js'));
+});
 
 // app.use('/', siteNavigation);
 app.use('/api/games', api);
@@ -30,8 +37,6 @@ app.use('/api/highscores', highscores);
 
 //konfigurera express, så att den ska leta efter statiska i frontendmappens build mapp
 // app.use(express.static('../frontend/build'));
-
-
 
 // app.get('/', async (req, res) => {
 //   res.send(await fs.readFile('./HTML/index.html'));
