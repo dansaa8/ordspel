1db344d527b38a845c834e26fc3b60e383d2b78c
Dan Saa <dansaa8@protonmail.com>
2023-04-24 15:25:13 +0200

    Fix

--- backend/src/routes/highscores.js
+++ backend/src/routes/highscores.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import Database from '../db/Database.js';
+const DB = new Database();
 
 const router = express.Router();
 export default router;
 
 router.get('/', async (req, res) => {
-    const highscores = await Database.getHighscores();
+    const highscores = await DB.getHighscores();
     //OK success response code, indicates that req is successful
     res.status(200).json({
         data: highscores,
